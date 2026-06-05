514b03fe520173f3878b106f4082153a50c8f49f
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2026-06-05 18:46:18 +0530

    Add customer form fields and connect customer data to MySQL

--- customers.py
+++ customers.py
@@ -1,10 +1,12 @@
 import customtkinter as ctk
-from tkinter import ttk
+from tkinter import ttk, messagebox
+from database.customer_db import CustomerDB
 
 class CustomersPage:
 
     def __init__(self, parent):
-
+        
+        self.customer_db = CustomerDB()
         title = ctk.CTkLabel(
             parent,
             text="Customer Management",
@@ -22,12 +24,24 @@ class CustomersPage:
         self.phone.grid(row=0, column=1, padx=10, pady=10)
 
         self.email = ctk.CTkEntry(form, placeholder_text="Email")
-        self.email.grid(row=1, column=0, padx=10, pady=10)
+        self.email.grid(row=0, column=2, padx=10, pady=10)
 
         self.gst = ctk.CTkEntry(form, placeholder_text="GST Number")
-        self.gst.grid(row=1, column=1, padx=10, pady=10)
+        self.gst.grid(row=0, column=3, padx=10, pady=10)
+        
+        self.address = ctk.CTkEntry(form, placeholder_text = "Address")
+        self.address.grid(row = 1, column = 0, padx=10, pady=10)
+        
+        self.city = ctk.CTkEntry(form, placeholder_text= "City")
+        self.city.grid(row=1, column =1, padx=10, pady=10)
+        
+        self.state = ctk.CTkEntry(form, placeholder_text = "State")
+        self.state.grid(row =1, column=2, padx=10, pady= 10)
+        
+        self.pincode = ctk.CTkEntry(form, placeholder_text ="Pincode")
+        self.pincode.grid(row = 1, column = 3, padx = 10, pady = 10)
 
-        add_btn = ctk.CTkButton(form, text="Add Customer")
+        add_btn = ctk.CTkButton(form, text="Add Customer", command =self.add_customer)
         add_btn.grid(row=2, column=0, pady=20)
 
         table_frame = ctk.CTkFrame(parent)
@@ -38,13 +52,58 @@ class CustomersPage:
             "Name",
             "Phone",
             "Email",
-            "GST"
+            "GST",
+            "Address",
+            "City",
+            "State",
+            "Pincode"
         )
 
-        tree = ttk.Treeview(table_frame, columns=columns, show="headings")
+        self.tree = ttk.Treeview(table_frame, columns=columns, show="headings")
 
         for col in columns:
-            tree.heading(col, text=col)
-            tree.column(col, width=150)
+            self.tree.heading(col, text=col)
+            self.tree.column(col, width=150)
 
-        tree.pack(fill="both", expand=True)
+        self.tree.pack(fill="both", expand=True)
+    
+    # ==========================================
+    # ADD CUSTOMER
+    # ==========================================
+
+    def add_customer(self):
+
+        try:
+
+            self.customer_db.add_customer(
+                customer_name=self.name.get(),
+                phone=self.phone.get(),
+                email=self.email.get(),
+                 gst_number=self.gst.get(),
+                address= self.address.get(),
+                city= self.city.get(),
+                state= self.state.get(),
+                pincode= self.pincode.get()
+               
+            )
+
+            messagebox.showinfo(
+                "Success",
+                "Customer added successfully!"
+            )
+
+            self.name.delete(0, "end")
+            self.phone.delete(0, "end")
+            self.email.delete(0, "end")
+            self.gst.delete(0, "end")
+            self.address.delete(0, "end")
+            self.city.delete(0, "end")
+            self.state.delete(0, "end")
+            self.pincode.delete(0, "end")
+
+        except Exception as e:
+
+            messagebox.showerror(
+                "Error",
+                str(e)
+            )
